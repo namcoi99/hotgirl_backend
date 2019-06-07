84cd3e335cc88323eaa7171017f7d59f9e36daa2
nam_45 <nam_1999_x@yahoo.com.vn>
2019-06-07 19:57:43 +0700

    first commit

---
+++ public/precache-manifest.707511518d4c794a3c90aa3c2fd8dd6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c0b5f83330b7dbc95c967b4a1471c8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a78d38a417f470d89d00",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "336bbc8e93c3af3d22a6",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a78d38a417f470d89d00",
+    "url": "/static/js/2.a78a635c.chunk.js"
+  },
+  {
+    "revision": "336bbc8e93c3af3d22a6",
+    "url": "/static/js/main.797e9de3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2b8e2d9eb9a4b67593b325751f18f497",
+    "url": "/static/media/logo.2b8e2d9e.png"
+  }
+]);
